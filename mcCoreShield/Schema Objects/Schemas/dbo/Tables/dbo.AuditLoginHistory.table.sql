0f737a13782dd1af6322b5d81e4e2a0339d54778
ccarson <chris.carson.sql@gmail.com>
2013-02-10 16:44:23 -0600

    Imported production mcCoreShield, converted to SSDT project

---
+++ mcCoreShield/Schema Objects/Schemas/dbo/Tables/dbo.AuditLoginHistory.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AuditLoginHistory] (
+    [id]         INT           IDENTITY (23377, 1) NOT NULL,
+    [user_id]    INT           DEFAULT ('0') NOT NULL,
+    [login_date] DATETIME2 (0) DEFAULT (NULL) NULL,
+    [systemID]   INT           DEFAULT ((0)) NOT NULL,
+    [appID]      INT           DEFAULT ((0)) NOT NULL
+);
+
---
+++ mcCoreShield/Schema Objects/Schemas/dbo/Tables/dbo.meetingsResponses.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[meetingsResponses] (
+    [id]           INT           IDENTITY (1, 1) NOT NULL,
+    [sResponse]    NVARCHAR (50) NOT NULL,
+    [bActive]      BIT           DEFAULT ((1)) NOT NULL,
+    [displayOrder] INT           NULL
+);
+
